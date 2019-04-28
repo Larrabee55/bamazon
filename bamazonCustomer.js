ca04b98ba30ec238b981538f88baac63399c8126
Cameron Larrabee <cameronlarrabee@yahoo.com>
2019-04-27 18:27:14 -0600

    added function to display products and ask user if they want to buy something or exit. If the User clicks buys an Item it runs a second function. It askes the user which item (by ID) then how many they want to buy. If the amount they want to buy is greater than the stock it tells them insufficent quanity and makes them chooose an product again, if not it calculates the amount and tells the user their total.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,11 +23,90 @@ connection.connect(function (err) {
 function displayItems() {
   connection.query("SELECT * FROM products", function (err, results) {
     if (err) throw err;
-
     var choiceArray = [];
     for (var i = 0; i < results.length; i++) {
       choiceArray.push("ID: " + results[i].id + " Product: " + results[i].product_name + " Price: $" + results[i].price);
     }
-    console.log(choiceArray);
-  })
+    console.log(choiceArray)
+
+    inquirer
+      .prompt([{
+        name: "choices",
+        type: "list",
+        choices: ["Buy an Item", "Exit"]
+      }]).then(function (answer) {
+
+        if ("Buy an Item") {
+          buyAnItem()
+        } else {
+          // exit
+        }
+      })
+  });
 }
+
+function buyAnItem() {
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+
+    inquirer
+      .prompt([{
+          name: "choice",
+          type: "rawlist",
+          choices: function () {
+            var choicesArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choicesArray.push(results[i].id);
+            }
+            return choicesArray;
+          },
+          message: "Which procuct would you like to purchase? (by ID)"
+        },
+        {
+          name: "quantityWanted",
+          type: "input",
+          message: "How many would you like to purchase?"
+        }
+      ]).then(function (answer) {
+        var chosenProduct;
+
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].id === answer.choice) {
+            chosenProduct = results[i];
+          }
+        }
+        console.log(chosenProduct.stock_quantity)
+        console.log(answer.quantityWanted)
+
+        if (chosenProduct.stock_quantity > parseInt(answer.quantityWanted)) {
+          var newQuantity = chosenProduct.stock_quantity - answer.quantityWanted;
+          var totalPurchase = answer.quantityWanted * chosenProduct.price;
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [{
+                stock_quantity: newQuantity
+              },
+              {
+                id: chosenProduct.id
+              }
+            ],
+            function (err) {
+              if (err) throw err;
+              console.log("Your total is $" + totalPurchase)
+              displayItems();
+            }
+          );
+        } else {
+
+          console.log("insufficient Quantity")
+          buyAnItem()
+        }
+      });
+  });
+}
+
+// {
+//   name: "Quantiy",
+//   tpye: "input",
+//   message: "How many do you want to buy?"
+// }
